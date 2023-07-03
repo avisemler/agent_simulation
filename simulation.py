4d94af23cf972896540f0112ac3c8fa5ebbb414b
Avi Semler <avi.semler@gmail.com>
2023-07-03 16:35:56 +0100

    add new action type where the right tail is Gaussian and left constant

--- simulation.py
+++ simulation.py
@@ -14,19 +14,24 @@ def gaussian_density(x, mu, sigma):
     return result * 10000
 
 class Action:
+    def __init__(self, name):
+        self.name = name
+
     def get_value(self) -> float:
         """Return the value of the action under current conditions"""
         pass
 
 class ConstantAction(Action):
-    def __init__(self, value):
+    def __init__(self, name, value):
+        super().__init__(name)
         self.value = value
 
     def get_value(self, *args):
         return self.value
 
 class CongestedConstantAction(Action):
-    def __init__(self, capacity, below_capacity_value, above_capacity_value):
+    def __init__(self, name, capacity, below_capacity_value, above_capacity_value):
+        super().__init__(name)
         self.capacity = capacity
         self.below_capacity_value = below_capacity_value
         self.above_capacity_value = above_capacity_value
@@ -38,7 +43,8 @@ class CongestedConstantAction(Action):
             return self.above_capacity_value
 
 class GaussianCongestedAction(Action):
-    def __init__(self, capacity, left_tail_sd, right_tail_sd):
+    def __init__(self, name, capacity, left_tail_sd, right_tail_sd):
+        super().__init__(name)
         self.capacity = capacity
         self.left_tail_sd = left_tail_sd
         self.right_tail_sd = right_tail_sd
@@ -47,6 +53,21 @@ class GaussianCongestedAction(Action):
         if args[0] <= self.capacity:
             #in-capacity case
             return gaussian_density(args[0], self.capacity, self.left_tail_sd)
+        else:
+            return gaussian_density(args[0], self.capacity, self.right_tail_sd)
+
+class RightGaussianCongestedAction(Action):
+    """Action where the right-tail uses gaussian density but the left is constant"""
+    def __init__(self, name, capacity, left_tail_constant, right_tail_sd):
+        super().__init__(name)
+        self.capacity = capacity
+        self.left_tail_constant = left_tail_constant
+        self.right_tail_sd = right_tail_sd
+
+    def get_value(self, *args):
+        if args[0] <= self.capacity:
+            #in-capacity case
+            return self.left_tail_constant
         else:
             return gaussian_density(args[0], self.capacity, self.right_tail_sd)
 
@@ -111,9 +132,9 @@ class Simulation:
 
 if __name__ == "__main__":
     Simulation(
-        actions=[GaussianCongestedAction(250, 300, 200),
-        GaussianCongestedAction(400, 150, 10),
-        GaussianCongestedAction(100, 800, 100)],
+        actions=[RightGaussianCongestedAction("1", 200, 4, 120),
+        RightGaussianCongestedAction("2", 500, 1, 10),
+        RightGaussianCongestedAction("3", 400, 3, 100)],
         agents=[400, 320],
         agent_parameters=[
             [(2, 0), (3, 0), (0.4, -4)],
@@ -121,4 +142,4 @@ if __name__ == "__main__":
         ],
         timesteps=200
     ).run()
-    
+
